45a3f590002b36977178caaf1c2525e377f9849a
Oleg Shipitko <shipitko@visillect.com>
2017-09-23 12:48:39 +0300

    .gitignore added

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/oleg_shipitko/truggy/src
-# Build directory: /home/oleg_shipitko/truggy/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
